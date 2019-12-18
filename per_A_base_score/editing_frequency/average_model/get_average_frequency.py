5f13ef7f430930ec0f2a9b8f3c276ca5440aff69
YichaoOU <yli11@splprhpc05.cm.cluster>
2019-12-18 16:43:12 -0600

    editing_frequency code

---
+++ per_A_base_score/editing_frequency/average_model/get_average_frequency.py
@@ -0,0 +1,80 @@
+
+
+"""
+python2
+
+"""
+
+import matplotlib
+import pandas as pd
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+import string
+import glob
+import numpy as np
+
+def revcomp(seq):
+	tab = string.maketrans(b"ACTG", b"TGAC")
+	return seq.translate(tab)[::-1]
+
+def get_ref_alt(input_gRNA,table_header_list):
+	table_header=[x.split(".")[0] for x in table_header_list]
+	table_header = "".join(table_header)
+	if input_gRNA == table_header:
+		return "A","G"
+	elif revcomp(input_gRNA) == table_header:
+		return "T","C"
+	else:
+		print ("something is wrong, Exist!")
+		exit()
+	
+
+def parse_df(x):
+	freq_table = "/".join(x.split("/")[:-1])+"/Quantification_window_nucleotide_percentage_table.txt"
+	gRNA = x.split("_")[-1].replace(".txt","")
+	df = pd.read_csv(freq_table,sep="\t",index_col=0)
+	# print (df.head())
+	my_freq = []
+	flag_type = []
+	ref,alt = get_ref_alt(gRNA,df.columns.tolist())
+	for c in df.columns:
+		base = c.split(".")[0]
+		freq = 0
+		if base == ref:
+			freq = df.at[alt,c]
+		my_freq.append(freq)
+	if ref == "T":
+		return my_freq[::-1]
+	return my_freq
+		
+			
+			
+		
+
+	
+
+inputs = pd.read_csv("input.list",header=None)
+inputs = inputs[0].tolist()
+
+
+df = pd.DataFrame([parse_df(i) for i in inputs])
+
+
+df.index = inputs
+# print (df.head())
+df = df.replace(0, np.NaN)
+df.to_csv("average_model.csv")
+
+avg = pd.DataFrame(df.mean())
+avg[1] = avg.index.tolist()
+print (avg)
+
+plt.figure(figsize=(5,2))
+sns.barplot(x=1,y=0,data=avg)
+plt.ylabel("Editing frequency")
+plt.xlabel('A position')
+plt.savefig("editing_frequecy_barplot.pdf", bbox_inches='tight')
+
+
+
